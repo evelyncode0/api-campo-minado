b2c24e741e026bad043561f0c92944d74c2527b5
Evelyn Gregorio <160793847+evelyncode0@users.noreply.github.com>
2026-06-17 21:07:52 -0300

    Delete teste.js

--- teste.js
+++
@@ -1,5 +0,0 @@
-// Teste criado pelo Guilherme para testar a logica do jogo
-const JogoService = require ("./src/services/JogoService");
-
-tabuleiro = JogoService.gerarTabuleiro();
-console.log(tabuleiro);
